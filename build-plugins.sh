bdac70d73dffc8c7fb77165f7529521c13dfbc28
starapo7348 <126355018+starapo7348@users.noreply.github.com>
2026-01-23 20:37:14 +0100

    Update build-plugins.sh -> Making Program versions configurable
    Program versions centralized at one place.
    Making Program versions configurable at config.txt
    for the following programs:
    
    -FFMPEG
    -NASM
    -ZIG
    
    Only need to a change a single value for a specific version.

--- build-plugins.sh
+++ build-plugins.sh
@@ -14,6 +14,7 @@
 #              build process.
 # -----------------------------------------------------------------------------
 
+#include ./config.txt
 set -euo pipefail
 
 s_begin=$(date "+%s")
@@ -44,7 +45,8 @@ handle_error() {
 cleanup() {
   print_message "Aufräumarbeiten werden durchgeführt..." "Clean-up work is carried out..."
   if [ -d build ]; then
-    rm -rf build
+    sleep 0
+#    rm -rf build
   fi
 }
 
@@ -77,7 +79,6 @@ install_system_packages() {
       libbluray-dev libpng-dev libjansson-dev python3-testresources libxxhash-dev \
       libturbojpeg0-dev python3-setuptools python3-wheel python-is-python3 \
       libxxhash-dev vulkan-validationlayers libvulkan1 g++-11 llvm-14 \
-      dos2unix \
       llvm-dev libgsl-dev libheif-dev \
       llvm-20-dev clang-20 libclang-20-dev lld-20 liblld-20 liblld-20-dev #zig Dependencies
 }
@@ -97,15 +98,14 @@ install_nasm() {
 # NASM - The Netwide Assembler https://www.nasm.us
 # ------------------------------------------------
   if [ ! -x "$VSPREFIX/bin/nasm" ]; then
-    ver="2.16.03"
-    wget -c "https://www.nasm.us/pub/nasm/releasebuilds/$ver/nasm-${ver}.tar.xz" || handle_error "Fehler beim Herunterladen von NASM." "Error downloading NASM."
-    tar xf "nasm-${ver}.tar.xz"
-    cd "nasm-$ver" || handle_error "Fehler beim Wechseln in das NASM-Verzeichnis." "Error changing to NASM directory."
+    wget -c "https://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/nasm-${NASM_VERSION}.tar.xz" || handle_error "Fehler beim Herunterladen von NASM." "Error downloading NASM."
+    tar xf "nasm-${NASM_VERSION}.tar.xz"
+    cd "nasm-${NASM_VERSION}" || handle_error "Fehler beim Wechseln in das NASM-Verzeichnis." "Error changing to NASM directory."
     ./configure --prefix="$VSPREFIX"
-    make -j"$JOBS"
+    make -j"${JOBS}"
     make install
     cd .. || handle_error "Fehler beim Zurückwechseln in das vorherige Verzeichnis." "Error changing back to the previous directory."
-    rm -rf "nasm-$ver" "nasm-${ver}.tar.xz"
+    rm -rf "nasm-${NASM_VERSION}" "nasm-${NASM_VERSION}.tar.xz"
   fi
 }
 
@@ -115,15 +115,14 @@ install_zig() {
 # ZIG https://codeberg.org/ziglang/zig
 # ------------------------------------
   if [ ! -x "$VSPREFIX/bin/zig" ]; then
-    ZIG_VERSION="0.15.2"
-    git clone --branch $ZIG_VERSION --depth 1 https://codeberg.org/ziglang/zig.git zig-$ZIG_VERSION || handle_error "Fehler beim Klonen des ZIG-Repos." "Error cloning ZIG-Repo."
-    cd zig-$ZIG_VERSION || handle_error "Fehler beim Wechseln in das ZIG-Verzeichnis." "Error changing to ZIG directory."
+    git clone --branch "${ZIG_VERSION}" --depth 1 https://codeberg.org/ziglang/zig.git "zig-${ZIG_VERSION}" || handle_error "Fehler beim Klonen des ZIG-Repos." "Error cloning ZIG-Repo."
+    cd "zig-${ZIG_VERSION}" || handle_error "Fehler beim Wechseln in das ZIG-Verzeichnis." "Error changing to ZIG directory."
     mkdir build
     cd build
     cmake .. -DCMAKE_INSTALL_PREFIX="$VSPREFIX" \
      -DCMAKE_PREFIX_PATH="$VSPREFIX" \
      -DCMAKE_BUILD_TYPE=Release \
-     -DZIG_VERSION="$ZIG_VERSION"
+     -DZIG_VERSION="${ZIG_VERSION}"
     cmake --build . --parallel "$(nproc)" --target install
     strip "$VSPREFIX/bin/zig" || true
     cd .. || handle_error "Fehler beim Zurückwechseln in das vorherige Verzeichnis." "Error changing back to the previous directory."
